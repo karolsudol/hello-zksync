349d13712ef72eda309049c24d4773939e3881d2
elcomendante <karol@finsane.com>
2023-01-25 13:23:22 +0000

    - Rtifacts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,53 +8,25 @@ dotenv.config();
 
 const GOERLI_URL = `https://goerli.infura.io/v3/${process.env.INFURA_KEY}`;
 
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
+
 module.exports = {
   zksolc: {
     version: "1.2.2",
     compilerSource: "binary",
     settings: {},
   },
-  //   defaultNetwork: "zkSyncTestnet",
+  defaultNetwork: "zkSyncTestnet",
+
   networks: {
-    testnet: {
-      zksync: true,
+    zkSyncTestnet: {
       url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli",
+      ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+      zksync: true,
     },
   },
-
-  //   networks: {
-  //     zkSyncTestnet: {
-  //       url: "https://zksync2-testnet.zksync.dev",
-  //       ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
-  //       //   ethNetwork: GOERLI_URL,
-  //       zksync: true,
-  //     },
-  //   },
   solidity: {
     version: "0.8.16",
   },
-  //   etherscan: {
-  //     apiKey: {
-  //       goerli: GOERLI_URL,
-  //     },
-  //     customChains: [
-  //       {
-  //         network: "zkSync-testnet",
-  //         chainId: 280,
-  //         zksync: true,
-  //         url: "https://zksync2-testnet.zksync.dev",
-  //         ethNetwork: "goerli",
-  //         // urls: {
-  //         //   apiURL: GOERLI_URL,
-  //         //   browserURL: "https://goerli.explorer.zksync.io/",
-  //         // },
-  //       },
-  //     ],
-  //   },
-
-  //   etherscan: {
-  //     apiKey: process.env.ETHERSCAN_API_KEY,
-  //     apiKey: process.env.BSCSCAN_API_KEY,
-  //   },
 };
